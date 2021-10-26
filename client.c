846c8b07990ec4b742cce05630244b5ed6975b96
Ardelean George <georgeardelean45@gmail.com>
2021-10-26 15:34:22 +0300

    Show table with steps

--- client.c
+++ client.c
@@ -13,7 +13,7 @@ int main(int argc,char* argv[]) {
     uint16_t c;
     c = socket(AF_INET, SOCK_STREAM, 0);
     if (c < 0) {
-        printf("Eroare la crearea socketului client\n");
+        printf("Error creating client's socket\n");
         return 1;
     }
 
@@ -23,7 +23,7 @@ int main(int argc,char* argv[]) {
     server.sin_addr.s_addr = inet_addr(argv[2]);
 
     if (connect(c, (struct sockaddr *) &server, sizeof(server)) < 0) {
-            printf("Eroare la conectarea la server\n");
+            printf("Error connecting to the server\n");
             return 1;
     }
 
@@ -62,23 +62,26 @@ int main(int argc,char* argv[]) {
     char status[30],addr[30];
     bzero(status, 30);
     recv(c, status, 30, 0);
-    printf("%s",status);
-
     bzero(addr, 30);
     recv(c, addr, 30, 0);
-    printf("For client %s\n",addr);
 
+
+    printf("For client %s\n",addr);
+    printf("Your game status: %s",status);
+    printf("\n");
+
+    printf("Stats for other games: \n");
     uint16_t nr_winners;
     recv(c, &nr_winners, 2, 0);
     nr_winners = ntohs(nr_winners);
     printf("Number of winners %hu\n", nr_winners);
 
-    
+    printf("List of the winners and their number of moves\n");
     for(int i = 0 ; i < nr_winners; i++){
         char winner[80];
         bzero(winner, 80);
         recv(c, winner, 80, 0);
-        printf("%s\n",winner);
+        printf("%s",winner);
     }
 
     close(c);
--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import socket
 import threading
 import struct
 import sys
+import os
+from time import sleep
 
 TCP_PORT =int(sys.argv[1])  
 TCP_IP = sys.argv[2]
@@ -14,12 +16,21 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((TCP_IP, TCP_PORT))
 
 server.listen(5)
- 
+
 def show_chessboard(board):
     for i in board:
         print(i)
 
-def horse_jump(row1,col1,row2,col2):
+def progress(board, movements,addr):
+    print(f"Printing moves for client {addr}")
+    for movement in movements:
+        sleep(2)
+        # os.system('clear')
+        print("----------------------------------------")
+        board[movement[0]][movement[1]] = str(movement[2])
+        show_chessboard(board)
+
+def horse_jump(row1,col1,row2,col2,board):
     #check if the positions are going in an L going 2 above or below 1 to the left or right
     if abs(row1 - row2 ) == 2:
         if abs(col1 -col2) ==1:
@@ -35,7 +46,7 @@ def horse_jump(row1,col1,row2,col2):
 def handle_client(conn, addr):
     print(f"Hello client {addr}")
     rows,cols = (8,8)
-    board = [[0 for i in range(cols)] for j in range(rows)]
+    board = [['0' for i in range(cols)] for j in range(rows)]
     # row_start = randint(0,7)
     # col_start = randint(0,7)
     # row_stop = randint(0,7)
@@ -52,7 +63,7 @@ def handle_client(conn, addr):
    
 
     positions = struct.pack('>HHHH',row_start,col_start,row_stop,col_stop)
-    conn.send(positions)
+    conn.send(positions)    
 
     nr = int.from_bytes(conn.recv(2), byteorder="big")
 
@@ -60,29 +71,27 @@ def handle_client(conn, addr):
     col1 = int.from_bytes(conn.recv(2), byteorder="big")
 
     #if the first jump is not valid
-    if not horse_jump(row_start,col_start,row1,col1):
+    if not horse_jump(row_start,col_start,row1,col1,board):
         print("The first jump is not good")
+    
 
     movements.append([row1,col1,1])
-    board[row1][col1] = 1
+    board[row1][col1] = '1'
     moves = 1
     for i in range(0,nr-1):
         row2 = int.from_bytes(conn.recv(2), byteorder="big")
         col2 = int.from_bytes(conn.recv(2), byteorder="big")
-        if not horse_jump(row1,col1,row2,col2):
+        if not horse_jump(row1,col1,row2,col2,board):
             break
         row1 = row2
         col1 = col2
         moves +=1
-        board[row1][col1] = moves
+        # board[row1][col1] = str(moves)
         movements.append([row1,col1,moves])
 
     if moves == nr:
-        if horse_jump(row1,col1,row_stop,col_stop):
+        if horse_jump(row1,col1,row_stop,col_stop,board):
             doing_well = True
-            print("Final board with the movement numbers")
-            show_chessboard(board)
-
             winners.append([addr[0],nr])
             print("This guy won :)")
         else:
@@ -90,30 +99,30 @@ def handle_client(conn, addr):
     else:
         print("Not all moves are correct :(")
 
-    
 
     if doing_well == True:
-        msg = "Good job you won :)\n"
-        conn.send(msg.encode())
+        game_status = "Good job you won :)\n"
+        conn.send(game_status.encode())
         
     else:
-        msg = "Sadly you lost :(\n"
-        conn.send(msg.encode())
+        game_status = "Sadly you lost :(\n"
+        conn.send(game_status.encode())
 
     ip_addr = str(addr[0])
     conn.send(ip_addr.encode())
 
     winners_no = len(winners)
-    print(winners_no)
     no_winners = struct.pack('>H', winners_no)
     conn.send(no_winners)
 
     for i in range(0,winners_no):
-        win = str("IP winner:" + winners[i][0]) + " Number of moves: " + str(winners[i][1])
-        print(win)
+        win = str("IP winner:" + winners[i][0]) + " Number of moves: " + str(winners[i][1]) + "\n"
         conn.send(win.encode())
+        win = ""
 
     conn.close()
+    progress(board,movements,addr)
+    print(game_status)
 
 while True:
     conn, addr = server.accept()
